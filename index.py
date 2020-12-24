0d812227dafe1c48196561e0c497b122ebf13f34
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-12-23 22:59:53 -0500

    Removed auto reply and added 15 min sleep on 420 error code

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import json
 import tweepy
+from time import *
 from os import environ
 
 APIKEY = environ['Consumer_Key']
@@ -19,12 +20,6 @@ class MyStreamListener(tweepy.StreamListener):
         if 'retweeted_status' in tweetInfo:
             return
 
-        # BLACC Publicize message
-        replyContent = "We're BLACC - The Black Coder Community, a safe space for black people to exchange knowledge and be exposed to new technologies. If you’re looking to join a community exemplifying Black excellence and pushing the needle within tech, click the link below! blacc.xyz/discord"
-
-        # Reply to the tweet
-        api.update_status(status=replyContent, in_reply_to_status_id=tweetInfo['id'],
-                          auto_populate_reply_metadata=True)
         # Like the tweet
         if not tweet.favorited:
             try:
@@ -34,14 +29,16 @@ class MyStreamListener(tweepy.StreamListener):
 
         # Retweet the tweet
         if not tweet.retweeted:
-            # Retweet, since we have not retweeted it yet
             try:
                 tweet.retweet()
             except Exception as e:
                 print("Error: " + str(e))
+
     def on_error(self, status):
         if status == 420:
             print("Error detected: " + str(status) + "\nClosing and reconnecting the Stream...")
+            # Wait 15 min before reconnecting
+            sleep(900)
             return False
 
 
@@ -54,4 +51,4 @@ api = tweepy.API(auth, wait_on_rate_limit=True,
     wait_on_rate_limit_notify=True)
 tweets_listener = MyStreamListener(api)
 stream = tweepy.Stream(api.auth, tweets_listener)
-stream.filter(track=["#blacktechtwitter","#blackintech","#blackcoders","#blacktechpipeline","#blackdesigners","#blackwomenintech"],is_async=True)
+stream.filter(track=["#blacktechtwitter","#blacc"],is_async=True)
